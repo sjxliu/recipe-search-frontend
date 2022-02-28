49918dade392e6971dac475cc93b0cfd4e5f4690
unknown <sjxliu8@gmail.com>
2022-02-28 11:20:09 -0600

    adding styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,10 @@
 import {Link} from 'react-router-dom'
+import "./styles/home.css"
 
 function Home(){
     return(
-        <div>
-            <h3>Welcome to our recipe search/save website. Here's a description of what we can do for you...</h3>
+    <div className='home-container'>
+            <h3 className='greeting'>Welcome to our recipe search/save website. Here's a description of what we can do for you...</h3>
             <div style={{display: 'flex', justifyContent: 'center'}}>
                 <Link to='/nameSearch'><button>Search a recipe by name</button></Link>
             </div>
