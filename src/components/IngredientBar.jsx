48cba9bc0a684bded160d4e4580039f9697c9c9b
unknown <sjxliu8@gmail.com>
2022-02-28 12:03:17 -0600

    search by indgres

--- src/components/IngredientBar.jsx
+++ src/components/IngredientBar.jsx
@@ -1,30 +1,50 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from "react";
+import "./styles/bar.css";
 
 function IngredientBar(props) {
-    let [query, setQuery] = useState('')
-    let [results, setResults] = useState([])
+  let [query, setQuery] = useState("");
+  let [results, setResults] = useState([]);
 
-    useEffect(() => {
-        if(query){
-            fetch(`https://api.spoonacular.com/food/ingredients/search?query=${query}&apiKey=${process.env.REACT_APP_API_KEY}&number=4`)
-            .then(res => res.json())
-            .then(res => setResults(res.results))
-        } else {
-            setResults([])
-        }
-    }, [query])
+  useEffect(() => {
+    if (query) {
+      fetch(
+        `https://api.spoonacular.com/food/ingredients/search?query=${query}&apiKey=${process.env.REACT_APP_API_KEY}&number=4`
+      )
+        .then((res) => res.json())
+        .then((res) => setResults(res.results));
+    } else {
+      setResults([]);
+    }
+  }, [query]);
 
-    const suggestions = results.map((result, index) => <p onClick={(e) => {
-        props.addIngredient(result.name)
-        setQuery(result.name)
-    }} style={{padding: '4px 0', border: '1px solid black', borderTop: '0', width: '255px', margin: 'auto'}} key={index}>{result.name}</p>)
+  const suggestions = results.map((result, index) => (
+    <p
+      onClick={(e) => {
+        props.addIngredient(result.name);
+        setQuery(result.name);
+      }}
+      key={index}
+      className="suggestions"
+    >
+      {result.name}
+    </p>
+  ));
 
-    return(
-        <div style={{position: 'relative', width: '258px', margin: '10px auto'}}>
-            <input style={{width: '250px'}} onChange={(e) => setQuery(e.target.value)} value={query}/>
-            <div style={{position: 'absolute', top: '25px', left: '0.4px', zIndex: 10, backgroundColor: 'white'}} onClick={(e) => e.target.parentNode.style = 'display: none'}>{suggestions ? suggestions : null}</div>
-        </div>
-    )
+  return (
+    <div className="div1">
+      <input
+        className="input"
+        onChange={(e) => setQuery(e.target.value)}
+        value={query}
+      />
+      <div
+        className="click"
+        onClick={(e) => (e.target.parentNode.style = "display: none")}
+      >
+        {suggestions ? suggestions : null}
+      </div>
+    </div>
+  );
 }
 
-export default IngredientBar
+export default IngredientBar;
